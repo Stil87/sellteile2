7860cf98078c57e0a7969bb0ad8832ee20213b51
tillschmidt <schmidt.till.mail@gmail.com>
2021-04-16 12:31:38 +0100

    add env variables

--- src/utils/firebaseConfig.ts
+++ src/utils/firebaseConfig.ts
@@ -6,16 +6,18 @@ import * as firebaseui from 'firebaseui'
 import 'firebase/storage';
 import 'firebase/firebase-auth'
 import 'firebase/firebase-firestore'
+import { preProcessFile } from "typescript";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBubrfwxvlnU1ntjR1PXCbR_2ylAC6CcOk",
-  authDomain: "sellteile.firebaseapp.com",
-  projectId: "sellteile",
-  storageBucket: "sellteile.appspot.com",
-  messagingSenderId: "41707276635",
-  appId: "1:41707276635:web:603c2de743aee3c806327e",
-  measurementId: "G-MYQELKHBQ3"
+
+  apiKey: process.env.REACT_APP_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket:process.env.REACT_APP_STORAGE_BUCKET ,
+  messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId:process.env.REACT_APP_APP_ID,
+  measurementId:process.env.REACT_APP 
 }
 
 
