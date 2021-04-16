a0226581a867e375b45c8afebaec2de920e36b1d
Till Schmidt <schmidt.till.mail@gmail.com>
2021-04-16 11:59:25 +0100

    Update firebaseConfig.ts
    deleted firebase key

--- src/utils/firebaseConfig.ts
+++ src/utils/firebaseConfig.ts
@@ -9,13 +9,7 @@ import 'firebase/firebase-firestore'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBubrfwxvlnU1ntjR1PXCbR_2ylAC6CcOk",
-  authDomain: "sellteile.firebaseapp.com",
-  projectId: "sellteile",
-  storageBucket: "sellteile.appspot.com",
-  messagingSenderId: "41707276635",
-  appId: "1:41707276635:web:603c2de743aee3c806327e",
-  measurementId: "G-MYQELKHBQ3"
+
 }
 
 
